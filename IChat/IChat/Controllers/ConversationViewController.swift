89a1b6ae55d32f24049fa73c3f9eb93b4ca5cefa
yasminmohsen <yasminmohsen1993@gmail.com>
2020-06-26 14:53:04 +0200

    add project

---
+++ IChat/IChat/Controllers/ConversationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IChat
+//
+//  Created by yasmin mohsen on 6/25/20.
+//  Copyright © 2020 yasmin mohsen. All rights reserved.
+//
+
+import UIKit
+
+class ConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IChat/IChat/Controllers/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  IChat
+//
+//  Created by yasmin mohsen on 6/26/20.
+//  Copyright © 2020 yasmin mohsen. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
